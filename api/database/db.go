c46745583a131f15b165beee3a493ceeaabea844
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2024-06-26 07:37:37 -0400

    add connect database function

---
+++ api/database/db.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// define the database as a global variable
+var DB *gorm.DB
+
+// connect to the database
+func ConnectDb() error {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("store.db"), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	log.Println("Connected to Database!")
+
+	// Migrate the schema
+	// Need to add all models to the migrate function after created
+	err = DB.AutoMigrate()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
